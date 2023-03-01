b35e0c0ae8202a16129b8a526a90e5eb9a067f6c
PracaOl <oliwer.pietrzycki@op.pl>
2023-03-01 14:25:26 +0100

    Adding Even More Epic Code

---
+++ src/Post.class.php
@@ -0,0 +1,23 @@
+<?php
+class Post {
+    static function upload(string $tempFileName) {
+        $targetDir = "img/";
+        $imgInfo = getimagesize($tempFileName);
+        if (!is_array($imgInfo)) {
+            die("ERROR: Przekazany plik nie jest obrazem!");
+        }
+        $randomNumber = rand(10000, 99999) . hrtime(true);
+        $hash = hash("sha256", $randomNumber);
+        $newFileName = $targetDir . $hash . ".webp";
+        if (file_exists($newFileName)) 
+        {
+            die("ERROR: Podany plik już istnieje!!");
+        }
+        $imageString = file_get_contents($tempFileName);
+        $gdImage = @imagecreatefromstring($imageString);
+        imagewebp($gdImage, $newFileName);
+
+    }
+}
+
+?>
---
+++ src/config.php
@@ -0,0 +1,5 @@
+<?php
+$db = new mysqli("localhost", "root","", "cmse");
+require("Post.class.php");
+
+?>
