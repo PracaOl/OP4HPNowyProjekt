0dd1b5ae80547b03500ad9f1d838956918ee7c51
PracaOl <oliwer.pietrzycki@op.pl>
2023-04-12 16:02:38 +0200

    FIXES NEEDED!

--- src/Post.class.php
+++ src/Post.class.php
@@ -4,14 +4,22 @@ class Post {
     private string $filename;
     private string $timestamp;
     private string $title;  //usun jak niedziala
+    private int $authorId;
+    private string $authorName;
 
-    function __construct(int $i, string $f, string $t, string $ti) {
+    function __construct(int $i, string $f, string $t, string $title, int $authorId) {
         $this->id = $i;
         $this->filename = $f;
         $this->timestamp = $t;
-        $this->title = $ti;  //usun jak niedziala
+        $this->title = $title;  //usun jak niedziala
+        $this->authorId = $authorId;
+        global $db;
+        $this->authorName = User::getNameById($this->authorId);
     }
 
+    public function getId() : int {
+        return $this->id;
+    }
     public function getFilename() : string {
         return $this->filename;
     }
@@ -23,6 +31,9 @@ class Post {
     public function getTitle() : string {
         return $this->title;
     }
+    public function getAuthorName() : string {
+        return $this->authorName;
+    }
 
     static function getLast() : Post {
         global $db;
@@ -30,13 +41,13 @@ class Post {
         $query->execute();
         $result = $query->get_result();
         $row = $result->fetch_assoc();
-        $p = new Post($row['id'], $row['filename'], $row['timestamp'], $row['title']);
+        $p = new Post($row['id'], $row['filename'], $row['timestamp'], $row['title'], $row['userId']);
         return $p;
     }
 
     static function getPage(int $pageNumber = 1, int $postsPerPage = 10) : array {
         global $db;
-        $query = $db->prepare("SELECT * FROM post ORDER BY timestamp DESC LIMIT ? OFFSET ?");
+        $query = $db->prepare("SELECT * FROM post WHERE removed = 0 ORDER BY timestamp DESC LIMIT ? OFFSET ?");
         $offset = ($pageNumber-1)*$postsPerPage;
         $query->bind_param('ii', $postsPerPage, $offset);
         $query->execute();
@@ -44,35 +55,40 @@ class Post {
         $postsArray = array();
         while($row = $result->fetch_assoc()) 
         {
-            $post = new Post($row['id'], $row['filename'], $row['timestamp'], $row['title']);
+            $post = new Post($row['id'], $row['filename'], $row['timestamp'], $row['title'], $row['userId']);
             array_push($postsArray, $post);
         }
         return $postsArray;
     }
 
-    static function upload(string $tempFileName) {
+    static function upload(string $tempFileName, string $title, int $userId) {
         $targetDir = "img/";
         $imgInfo = getimagesize($tempFileName);
-        if (!is_array($imgInfo)) {
-            die("ERROR: Przekazany plik nie jest obrazem!");
+        if(!is_array($imgInfo)) {
+            die("BŁĄD: Przekazany plik nie jest obrazem!");
         }
         $randomNumber = rand(10000, 99999) . hrtime(true);
         $hash = hash("sha256", $randomNumber);
         $newFileName = $targetDir . $hash . ".webp";
-        if (file_exists($newFileName)) 
-        {
-            die("ERROR: Podany plik już istnieje!!");
+        if(file_exists($newFileName)) {
+            die("BŁĄD: Podany plik już istnieje!");
         }
         $imageString = file_get_contents($tempFileName);
         $gdImage = @imagecreatefromstring($imageString);
         imagewebp($gdImage, $newFileName);
+
         global $db;
-        $query = $db->prepare("INSERT INTO post VALUES(NULL, ?, ?, ?)");
+        $query = $db->prepare("INSERT INTO post VALUES(NULL, ?, ?, ?, ?)");
         $dbTimestamp = date("Y-m-d H:i:s");
-        $titleName = $_POST["memeTitle"];
-        $query->bind_param("sss", $dbTimestamp, $newFileName, $titleName);
-            if(!$query->execute())
-                die("Błąd zapisu do bazy danych");
+        $query->bind_param("sssi", $dbTimestamp, $newFileName, $title, $userId);
+        if(!$query->execute())
+            die("Błąd zapisu do bazy danych");
+    }
+    public static function remove($id) : bool {
+        global $db;
+        $query = $db->prepare("UPDATE post SET removed = 1 WHERE id = ?");
+        $query->bind_param("i", $id);
+        return $query->execute();
     }
 }
 
---
+++ src/User.class.php
@@ -0,0 +1,67 @@
+<?php
+class User {
+        private int $id;
+        private string $email;
+
+
+    public function __construct(int $id, string $email) {
+        $this->id = $id;
+        $this->email = $email;
+    
+    }
+
+    public function getId() : int {
+        return $this->id;
+    }
+    public function getName() : string {
+        return $this->email;
+    }
+
+    public static function getNameById(int $userId) : string {
+        global $db;
+        $query = $db->prepare("SELECT email FROM user WHERE id = ? LIMIT 1");
+        $query->bind_param('i', $userId);
+        $query->execute();
+        $result = $query->get_result();
+        $row = $result->fetch_assoc();
+        return $row['email'];
+    }
+
+    public static function register(string $email, string $password) : bool {
+        global $db;
+        $query = $db->prepare("INSERT INTO user VALUES (NULL, ?, ?)");
+        $passwordHash = password_hash($password, PASSWORD_ARGON2I);
+        $query->bind_param('ss', $email, $passwordHash);
+        return $query->execute();
+    }
+
+    public static function login(string $email, string $password) : bool {
+        global $db;
+        $query = $db->prepare("SELECT * FROM user WHERE login = ? LIMIT 1");
+        $query->bind_param('s', $email);
+        $query->execute();
+        $result = $query->get_result();
+        $row = $result->fetch_assoc();
+        $passwordHash = $row['password'];
+        if(password_verify($password, $passwordHash)) {
+            $u = new User($row['id'], $email);
+            $_SESSION['user'] = $u;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static function isAuth() : bool {
+        if(isset($_SESSION['user'])) {
+            if($_SESSION['user'] instanceof User) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
+
+?>
